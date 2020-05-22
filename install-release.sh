10a59ac3f7090b65c2b5cb63d046de6fbb230522
Colette Contreras <ColetteContreras@outlook.com>
2020-05-22 17:35:01 +0800

    update install-script

--- install-release.sh
+++ install-release.sh
@@ -270,14 +270,12 @@ installV2Ray(){
         return 1
     fi
     makeExecutable v2ray
-    copyFile geoip.dat
-    copyFile geosite.dat
 
     # Install V2Ray server config to /etc/v2ray
     if [[ ! -f "/etc/v2ray/config.json" ]]; then
         mkdir -p /etc/v2ray
         mkdir -p /var/log/v2ray
-        cp "${VSRC_ROOT}/vpoint_vmess_freedom.json" "/etc/v2ray/config.json"
+        cp "${VSRC_ROOT}/config.json" "/etc/v2ray/config.json"
         if [[ $? -ne 0 ]]; then
             colorEcho ${YELLOW} "Failed to create V2Ray configuration file. Please create it manually."
             return 1
